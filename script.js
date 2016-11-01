28ed8acad819246e588266352ae23e51c0eded18
ChadKreutzer <kanthalion@gmail.com>
2016-10-31 22:39:30 -0600

    Implement challenge 4

--- script.js
+++ script.js
@@ -1,8 +1,13 @@
 $(document).ready(function() {
-    for (var i = 1; i <= (16 * 16); i++) {
-        $('#container').append('<div class="square"></div>');
-    }
-    $('.square').hover(function(){
-        $(this).css({'opacity': '.5'});
-    });
+
+  for (var i = 1; i <= (16 * 16); i++) {
+    $('#container').append('<div class="square"></div>');
+  }
+
+  $('.square').hover(function(){
+    var currentOpacity = +$(this).css('opacity');
+    if (currentOpacity < 1) currentOpacity+= .10;
+    $(this).css({'opacity': currentOpacity});
+  });
+
 });
