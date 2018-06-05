f5107d5eb92cbd67a2727875e2daa538ceb188f3
Chad Kreutzer <kanthalion@gmail.com>
2018-06-05 12:13:58 +0000

    refactored out jQuery and added styling

--- script.js
+++ script.js
@@ -1,36 +1,51 @@
-$(document).ready(function() {
+createGrid(16);
+const button = document.getElementById("new-grid");
 
-  createGrid(16);
-  $('.square').hover(colorIn);
+button.addEventListener("click", function() {
+    const squaresPerSide = prompt("How many squares per side?", 16);
 
-  $('button').on('click', function(){
-    $('.square').css({'opacity': '.1'});
-    var squaresPerSide = prompt("How many squares per side?", 16);
-
-    if(squaresPerSide != null){
-      if(squaresPerSide < 1 || squaresPerSide > 100){
-       alert("Number out of range. Try again."); 
-      }
-      else {
-        var squareSize = (800 / squaresPerSide);
-
-        createGrid(squaresPerSide);      
-        $('.square').css({'height': squareSize + 'px', 'width': squareSize + 'px'});
-      }   
-    }    
-    $('.square').hover(colorIn);   
-  });
-
-  function colorIn(){
-    var currentOpacity = +$(this).css('opacity');
-    if (currentOpacity < 1) currentOpacity+= .10;
-    $(this).css({'opacity': currentOpacity});
-  }
-  function createGrid(sqPsd) {
-    var numSquares = sqPsd * sqPsd;
-    $('#container').empty();
-    for (var i = 1; i <= numSquares; i++) {
-      $('#container').append('<div class="square"></div>');
+    if (squaresPerSide != null) {
+        if (squaresPerSide < 1 || squaresPerSide > 100) {
+            alert("Number out of range. Try again.");
+        }
+        else {
+            createGrid(squaresPerSide);
+        }
     }
-  }
 });
+
+
+function createGrid(squaresPerSide) {
+    const container = document.getElementById("container");
+    const squareSize = 800 / squaresPerSide;
+    const numberOfSquares = squaresPerSide * squaresPerSide;
+
+    while (container.firstChild) {
+        container.removeChild(container.firstChild);
+    }
+
+    for (let i = 1; i <= numberOfSquares; i++) {
+        const div = document.createElement("div");
+
+        div.classList = "square";
+        div.setAttribute("style",
+            `
+            border: none;
+            margin: 0;
+            padding: 0;
+            height: ${squareSize}px;
+            width: ${squareSize}px;
+            background-color: black;
+            opacity: .1;
+            display: inline-block;
+        `);
+        div.addEventListener("mouseenter", colorIn);
+        container.appendChild(div);
+    }
+}
+
+function colorIn() {
+    let currentOpacity = +this.style.opacity;
+    if (currentOpacity < 1) currentOpacity += .10;
+    this.style.opacity = currentOpacity;
+}
