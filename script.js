b2c25d0d90162e20003b9e0699c65ebb18e7096c
Chad Kreutzer <kanthalion@gmail.com>
2018-06-06 07:37:33 +0000

    fix container width issue (it feels like a kludge)

--- script.js
+++ script.js
@@ -1,35 +1,36 @@
-createGrid(16);
+const backing = document.getElementById("backing");
+backing.style.width = "700px";
+createGrid(16, backing);
+
 const button = document.getElementById("new-grid");
 
 button.addEventListener("click", function() {
-    const squaresPerSide = prompt("How many squares per side?", 16);
+  const squaresPerSide = prompt("How many squares per side?", 16);
 
-    if (squaresPerSide != null) {
-        if (squaresPerSide < 1 || squaresPerSide > 100) {
-            alert("Number out of range. Try again.");
-        }
-        else {
-            createGrid(squaresPerSide);
-        }
+  if (squaresPerSide != null) {
+    if (squaresPerSide < 1 || squaresPerSide > 100) {
+      alert("Number out of range. Try again.");
+    } else {
+      createGrid(squaresPerSide, backing);
     }
+  }
 });
 
+function createGrid(squaresPerSide, container) {
+  const squareSize = +container.style.width.slice(0, -2) / squaresPerSide;
+  const numberOfSquares = squaresPerSide * squaresPerSide;
 
-function createGrid(squaresPerSide) {
-    const container = document.getElementById("container");
-    const squareSize = +container.style.width / squaresPerSide;
-    const numberOfSquares = squaresPerSide * squaresPerSide;
+  while (container.firstChild) {
+    container.removeChild(container.firstChild);
+  }
 
-    while (container.firstChild) {
-        container.removeChild(container.firstChild);
-    }
+  for (let i = 1; i <= numberOfSquares; i++) {
+    const div = document.createElement("div");
 
-    for (let i = 1; i <= numberOfSquares; i++) {
-        const div = document.createElement("div");
-
-        div.classList = "square";
-        div.setAttribute("style",
-            `
+    div.classList = "square";
+    div.setAttribute(
+      "style",
+      `
             border: none;
             margin: 0;
             padding: 0;
@@ -38,14 +39,16 @@ function createGrid(squaresPerSide) {
             background-color: black;
             opacity: .1;
             display: inline-block;
-        `);
-        div.addEventListener("mouseenter", colorIn);
-        container.appendChild(div);
-    }
+        `
+    );
+
+    container.appendChild(div);
+    div.addEventListener("mouseenter", colorIn);
+  }
 }
 
 function colorIn() {
-    let currentOpacity = +this.style.opacity;
-    if (currentOpacity < 1) currentOpacity += .10;
-    this.style.opacity = currentOpacity;
+  let currentOpacity = +this.style.opacity;
+  if (currentOpacity < 1) currentOpacity += 0.1;
+  this.style.opacity = currentOpacity;
 }
